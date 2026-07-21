09e477711e4ae8ea8ab31e05ed2d7757fd9b7095
Marwan <marwanramizm77@gmail.com>
2026-07-21 17:39:52 +0530

    Revised mongodb url and done complete backend of this project verified with Postman

--- expense-tracker/src/main/java/com/marwan/ExpenseTrackerApplication.java
+++ expense-tracker/src/main/java/com/marwan/ExpenseTrackerApplication.java
@@ -1,13 +1,31 @@
 package com.marwan;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ExpenseTrackerApplication {
+public class ExpenseTrackerApplication implements CommandLineRunner {
+
+	@Value("${spring.data.mongodb.uri}")
+	private String mongoUri;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		System.out.println(">>> Loaded MongoDB URI: " + mongoUri);
+	}
+
+	// Explicitly create MongoClient bean so Spring Boot uses Atlas URI
+	@Bean
+	public MongoClient mongoClient() {
+		return MongoClients.create(mongoUri);
+	}
 }
