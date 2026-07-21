5282008345021bddb285458e57b8ac2cdae59796
Marwan <marwanramizm77@gmail.com>
2026-07-21 14:07:45 +0530

    AuthController done

--- expense-tracker/src/main/java/com/marwan/controller/AuthController.java
+++ expense-tracker/src/main/java/com/marwan/controller/AuthController.java
@@ -1,3 +1,29 @@
 package com.marwan.controller;
 import com.marwan.model.User;
-import com.marwan.service.
+import com.marwan.service.UserService;
+import com.marwan.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired private UserService userService;
+    @Autowired private JwtUtil jwtUtil;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        return userService.save(user);
+    }
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+        Optional<User> existing = userService.findByUsername(user.getUsername());
+        if(existing.isPresent() && existing.get().getPassword().equals(user.getPassword())){
+            return jwtUtil.generateToken(user.getUsername());
+        }
+        throw new RuntimeException("Invalid credentials");
+
+    }
+
+}
